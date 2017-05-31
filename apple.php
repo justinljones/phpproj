404df46609fe55ff1b51e72bdff7e1f981269316
JJones <jjones@brysontech.com>
2017-05-31 13:20:24 -0400

    Getters and Setters. Working on objects and their properties.

---
+++ apple.php
@@ -0,0 +1,17 @@
+<?php
+
+class apple{
+    protected $color;
+    protected $weight;
+    protected $size;
+    
+    public function setColor($color) {$this->color = $color; return $this;}
+    public function getColor(){ return $this->color; }
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php echo "Test" 
+<?php echo "Test";
+ include('apple.php');
+ $apple = new apple();
+ $apple -> setColor('Red');
+ echo "This apple's color is ".$apple->getColor();
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
