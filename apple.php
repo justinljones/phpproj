6e657560ea44c4810a9aae87c0ff2518e3c167c7
JJones <jjones@brysontech.com>
2017-06-02 11:48:28 -0400

    Seeds Commenting on lines for better understanding.

--- apple.php
+++ apple.php
@@ -1,23 +1,36 @@
 <?php
 
+//creating a new object (which is also a class)
 class apple{
+    //initializes two properties (both protected)
     protected $color;
+    //this is a property that is an Array (list, list, list)
     protected $seeds=Array();
     
     //Functions for $color
+    //this function sets the color variable.
     public function setColor($color) {
+        //NOT SURE HERE; Best I can tell, the function is retrieving a color, then
+        //the "this->color" part will take the value of "$color" and apply it to the 
+        //property of "apple"
         $this->color = $color; return $this;
         
     }
+    //this function will retrieve the color from the apple object (as it currently 
+    //stands?)
     public function getColor(){ return $this->color; }
     
     //Functions for $seeds
+    //NOT SURE HERE; This function seems similar to color but I don't quite understand
+    //what (Seeds $seeds) is doing
     public function setSeeds(Seeds $seeds) {
         $this->seeds = $seeds; return $this;
     }
     
     public function getSeeds() { return $this->seeds; }
     
+    //NOT SURE HERE; this function is adding seeds by...
+    //...
     public function addSeeds(Seeds $seeds){
         $this->seeds[] = $seeds; return $seeds;
     }
--- index.php
+++ index.php
@@ -4,15 +4,23 @@
  $apple -> setColor('Red');
  echo "This apple's color is ".$apple->getColor();
  include('seeds.php');
+ //this line initializes a new "empty" variable, ready to be assigned.
  $seeds=new seeds();
+ //this line sets that variable's value
  $seeds->setName('Johnny');
+ //This line, takes my overall object "apples" and adds the new seed to it
  $apple->addSeeds($seeds);
+ //this line creates a variable whose value is the result of the called function
+ //"getSeeds();" from the "apple" object.
  $newSeeds=$apple->getSeeds();
 
+ //See above
+ $seeds=new seeds();
  $seeds->setName('James');
  $apple->addSeeds($seeds);
  $newSeeds=$apple->getSeeds(); 
  
+ //this function loops through "newSeeds" and spits out the resulting name(s).
  foreach($newSeeds as $seed){
      echo "Seed name: " . $seed->getName();
  }
